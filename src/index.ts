b9def300f6236043cd853797403b81645f381154
Wojciech Bartnik <yisonPylkita@gmail.com>
2019-02-13 14:28:11 +0100

    Naiva approach to path mapping

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
-export function uriWslToWindows(wslUri: String) {
-  return "";
+export function uriWslToWindows(wslUri: string): string {
+  let uriSegments = wslUri.split('/');
+  if (uriSegments.length < 4 || uriSegments[0].length != 0 || uriSegments[1] != 'mnt') {
+    return '';
+  }
+
+  let disc_letter = uriSegments[2];
+  if (!/^[a-zA-Z]+$/.test(disc_letter)) {
+    return '';
+  }
+
+  let uriWindows = disc_letter + ':';
+  uriSegments.forEach(pathPart => {
+    uriWindows += '\\' + pathPart;
+  });
+  return uriWindows;
 }
 
-export function uriWindowsToWsl(windowsUri: String) {
-  return "";
+export function uriWindowsToWsl(windowsUri: string): string {
+  let uriSegments = windowsUri.split('\\');
+  if (uriSegments.length < 2) {
+    return '';
+  }
+
+  let disc_letter = uriSegments[0];
+  if (!/^[a-zA-Z]+$/.test(disc_letter)) {
+    return '';
+  }
+
+  let uriWsl = '/mnt/' + disc_letter;
+  uriSegments.forEach(pathPart => {
+    uriWsl += '/' + pathPart;
+  });
+  return uriWsl;
 }
